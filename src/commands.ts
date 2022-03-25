98ebbaf052185bb3ac4cba1ca1bd126431a7b5e6
ls3205 <ls3205@trevor.org>
2022-03-25 14:31:20 -0400

    Got basic structure and pagination working

--- src/commands.ts
+++ src/commands.ts
@@ -86,8 +86,109 @@ export async function hourly(message, zipcode) {
                     { name: 'Weather', value: `${currentWeather} (${currentDescription})`, inline: true },
                 )
                 .setAuthor({ name: `OpenWeatherMap`, iconURL: 'https://openweathermap.org/img/wn/' + current.weather[0].icon + '@2x.png' })
-            
-            message.channel.send({ embeds: [embed] });
+                .setFooter('Page 1 of 4');
+
+            const embed2 = new DiscordJS.MessageEmbed()
+                .setColor('#03adfc')
+                .setTitle(`Hourly Weather`)
+                .setDescription(`Weather in [${city}](https://www.google.com/maps/place/${lat},${lon}):`)
+                .setAuthor({ name: `OpenWeatherMap`, iconURL: 'https://openweathermap.org/img/wn/' + current.weather[0].icon + '@2x.png' })
+                .setFooter('Page 2 of 4');
+
+            const embed3 = new DiscordJS.MessageEmbed()
+                .setColor('#03adfc')
+                .setTitle(`Hourly Weather`)
+                .setDescription(`Weather in [${city}](https://www.google.com/maps/place/${lat},${lon}):`)
+                .setAuthor({ name: `OpenWeatherMap`, iconURL: 'https://openweathermap.org/img/wn/' + current.weather[0].icon + '@2x.png' })
+                .setFooter('Page 3 of 4');
+
+            const embed4 = new DiscordJS.MessageEmbed()
+                .setColor('#03adfc')
+                .setTitle(`Hourly Weather`)
+                .setDescription(`Weather in [${city}](https://www.google.com/maps/place/${lat},${lon}):`)
+                .setAuthor({ name: `OpenWeatherMap`, iconURL: 'https://openweathermap.org/img/wn/' + current.weather[0].icon + '@2x.png' })
+                .setFooter('Page 4 of 4');
+
+            const hourly = responseJSON.hourly;
+            for (let i = 1; i < 7; i++) {
+                const time = new Date((hourly[i].dt) * 1000);
+                const timeString = time.toLocaleTimeString();
+                const temp = kelToFahr(hourly[i].temp);
+                const feelsLike = kelToFahr(hourly[i].feels_like);
+                const weather = hourly[i].weather[0].main;
+                const description = hourly[i].weather[0].description;
+
+                embed.addFields(
+                    { name: `Time`, value: `${timeString}`, inline: true },
+                    { name: `Temperature`, value: `${temp}°F (Feels like: ${feelsLike}°F)`, inline: true },
+                    { name: 'Weather', value: `${weather} (${description})`, inline: true },
+                )
+            }
+            for (let i = 7; i < 13; i++) {
+                const time = new Date((hourly[i].dt) * 1000);
+                const timeString = time.toLocaleTimeString();
+                const temp = kelToFahr(hourly[i].temp);
+                const feelsLike = kelToFahr(hourly[i].feels_like);
+                const weather = hourly[i].weather[0].main;
+                const description = hourly[i].weather[0].description;
+
+                embed2.addFields(
+                    { name: `Time`, value: `${timeString}`, inline: true },
+                    { name: `Temperature`, value: `${temp}°F (Feels like: ${feelsLike}°F)`, inline: true },
+                    { name: 'Weather', value: `${weather} (${description})`, inline: true },
+                )
+            }
+            for (let i = 13; i < 19; i++) {
+                const time = new Date((hourly[i].dt) * 1000);
+                const timeString = time.toLocaleTimeString();
+                const temp = kelToFahr(hourly[i].temp);
+                const feelsLike = kelToFahr(hourly[i].feels_like);
+                const weather = hourly[i].weather[0].main;
+                const description = hourly[i].weather[0].description;
+
+                embed3.addFields(
+                    { name: `Time`, value: `${timeString}`, inline: true },
+                    { name: `Temperature`, value: `${temp}°F (Feels like: ${feelsLike}°F)`, inline: true },
+                    { name: 'Weather', value: `${weather} (${description})`, inline: true },
+                )
+            }
+            for (let i = 19; i < 25; i++) {
+                const time = new Date((hourly[i].dt) * 1000);
+                const timeString = time.toLocaleTimeString();
+                const temp = kelToFahr(hourly[i].temp);
+                const feelsLike = kelToFahr(hourly[i].feels_like);
+                const weather = hourly[i].weather[0].main;
+                const description = hourly[i].weather[0].description;
+
+                embed4.addFields(
+                    { name: `Time`, value: `${timeString}`, inline: true },
+                    { name: `Temperature`, value: `${temp}°F (Feels like: ${feelsLike}°F)`, inline: true },
+                    { name: 'Weather', value: `${weather} (${description})`, inline: true },
+                )
+            }
+
+            const pages = [embed, embed2, embed3, embed4];
+            var page = 0;
+
+            message.channel.send({embeds: [pages[page]]}).then(hourlyEmbeded => {
+                hourlyEmbeded.react('⏪');
+                hourlyEmbeded.react('⏩');
+
+                const filter = (reaction, user) => ['⏪', '⏩'].includes(reaction.emoji.name) && (user.id === message.author.id);
+                const collector = hourlyEmbeded.createReactionCollector(filter);
+
+                collector.on('collect', async (reaction, user) => {
+                    if (reaction.emoji.name === '⏪') {
+                        page = (page === 0) ? pages.length - 1 : page - 1;
+                    } else if (reaction.emoji.name === '⏩') {
+                        page = (page === pages.length - 1) ? 0 : page + 1;
+                    }
+                    await hourlyEmbeded.edit({embeds: [pages[page]]});
+                    if (user.id !== client.user?.id) {
+                        reaction.users.remove(user);
+                    }
+                });
+            });
         });
     });
 };
